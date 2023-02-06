d37bb3a5dbba8c99b787e4e9a5da6d93de59ba3d
JonasThers <jonasthers1992@gmail.com>
2023-02-06 21:13:06 +0200

    Add model for characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_05_123223) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_191208) do
   create_table "character_classes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
