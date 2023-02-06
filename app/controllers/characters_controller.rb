721b578ea371b7ac6902e91b9cb071d6e059d3f0
JonasThers <jonasthers1992@gmail.com>
2023-02-06 21:37:45 +0200

    Add CRUD functionality for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,50 @@
 class CharactersController < ApplicationController
+    def index
+        @characters = Character.all
+    end
+
+    def show
+        @character = Character.find(params[:id])
+    end
+
+    def new
+        @character = Character.new
+    end
+    
+    def create
+        @character = Character.new(characters_params)
+    
+        if @character.save
+          redirect_to characters_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+    
+    def edit
+        @character = Character.find(params[:id])
+    end
+    
+    def update
+        @character = Character.find(params[:id])
+    
+        if @character.update(characters_params)
+          redirect_to characters_path
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @character = Character.find(params[:id])
+        @character.destroy
+    
+        redirect_to characters_path, status: :see_other
+    end
+    
+    private
+        def characters_params
+          params.require(:character).permit(:name)
+        end
+    
 end
