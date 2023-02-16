7ff75ea8150115f75c90be3744961d0ae78f4a5d
JonasThers <jonasthers1992@gmail.com>
2023-02-16 10:56:36 +0200

    Add content to front page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    @classes_count = CharacterClass.all.count
+    @characters_count = Character.all.count
+    @parties_count = Party.all.count
   end
 end
